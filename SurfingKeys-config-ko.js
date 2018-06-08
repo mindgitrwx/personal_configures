e2f16f930dc23bf1330e2b2fbb4d06fea70d7a15
mindgitrwx <hybridkernal@gmail.com>
2018-06-09 08:40:44 +0900

    test

--- SurfingKeys-config-ko.js
+++ SurfingKeys-config-ko.js
@@ -424,13 +424,13 @@ mapkey('D', 'stackoverflow 답변 다운 스크롤', function () {
     stackAnswer++;
     pageHeadLine = document.querySelectorAll(".answer");
     pageHeadLine[stackAnswer].scrollIntoView();
-    window.scrollBy(0, -47); // Adjust scrolling with a negative value here
+    window.scrollBy(0, -47); // Adjust scrolling with a negative value : stackoverflow upper bar
 },{domain: /stackoverflow\.com/i});
 mapkey('U', 'stackoverflow 답변 up 스크롤 ', function () {
     stackAnswer--;
     pageHeadLine = document.querySelectorAll(".answer");
     pageHeadLine[stackAnswer].scrollIntoView();
-    window.scrollBy(0, -47); // Adjust scrolling with a negative value here
+    window.scrollBy(0, -47); // Adjust scrolling with a negative value here : stackoverflow upper bar
 },{domain: /stackoverflow\.com/i});
 
 // md 파일의 목차는 <h1> <h2> <h3> 이런 방식으로 결정되고, 숫자가 클수록 대단위의 목차다
