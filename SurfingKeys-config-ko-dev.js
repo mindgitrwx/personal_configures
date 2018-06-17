245e6518f751fed5fce7cfea17c23f069ebde80f
mindgitrwx <hybridkernal@gmail.com>
2018-06-17 20:58:02 +0900

    added pdf marking test

--- SurfingKeys-config-ko-dev.js
+++ SurfingKeys-config-ko-dev.js
@@ -1202,7 +1202,6 @@ addPDFVIMark = function (mark) {
     }
 };
 
-mapkey('m', '#10Add current PDF URL to vim-like marks', Normal.addPDFVIMark, {domain: /web\.kamihq\.com/i});
 
 jumpPDFVIMark = function (mark) {
     if (localPDFMarks.hasOwnProperty(mark)) {
@@ -1210,4 +1209,5 @@ jumpPDFVIMark = function (mark) {
         document.getElementById('pageNumber').value = localPDFMarks[mark].pageNumber;
     }
 };
-mapkey("'", '#10Jump to PDF vim-like mark', Normal.addPDFVIMark, {domain: /web\.kamihq\.com/i});
+mapkey('m', '#10Add current PDF URL to vim-like marks', Normal.addPDFVIMark, {domain: /web\.kamihq\.com/i});
+mapkey("'", '#10Jump to PDF vim-like mark', Normal.jumpPDFVIMark, {domain: /web\.kamihq\.com/i});
