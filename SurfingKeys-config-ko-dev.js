229ad9664e8e5136e02456cd99dd8ffdd99e927f
mindgitrwx <hybridkernal@gmail.com>
2018-06-19 22:59:30 +0900

    add - gc to queryselctor all

--- SurfingKeys-config-ko-dev.js
+++ SurfingKeys-config-ko-dev.js
@@ -953,7 +953,7 @@ mapkey('gm', "#7 goto memo element", function () {
 });
 // TODO: 동작 확인하기 - 동작하지 않음 
 mapkey('gc', "#7 goto comment element", function () {
-    documents.querySelectorAll('*comment*')[0].click(); // focus, comment?
+    documents.querySelectorAll('textarea')[0].focus(); // focus, comment?
 });
 // 
 
