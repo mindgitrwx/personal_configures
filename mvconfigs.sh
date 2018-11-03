bb2c80b473ce0d635d4e9c08449656536042f1d2
jonghyeon Yeo <hybridkernal@gmail.com>
2018-11-04 07:06:39 +0900

    add - surfingkeys mv

--- mvconfigs.sh
+++ mvconfigs.sh
@@ -2,6 +2,7 @@ XKBOPTIONS=ctrl:nocaps >> /etc/default/keyboard
 cp -r  nvim ~/.config/nvim
 cp -r  settings.json ~/.config/Code/User/settings.json
 cp -r  .spacemacs ~/.spacemacs
+cp -r  Surfingkeys-config-ko.js ~/.surfingconf.js
 cp -r  .zshrc ~/.zshrc
 cp -r  .ideavimrc ~/.ideavimrc
 cp -r  settings.json ~/.config/Code/User/settings.json
