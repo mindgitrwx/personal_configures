11bd424b004ea0c93786856a96fb21f2cc68c34e
jonghyeonYeo <hybridkernal@gmail.com>
2018-11-09 07:08:14 +0900

    add - work like window function

--- mvconfigs.sh
+++ mvconfigs.sh
@@ -17,6 +17,10 @@ vim -c 'PluginInstall' -c 'qa!'
 # function key behavior change like window
 sudo su -c "echo -n 0x02 > /sys/module/hid_apple/parameters/fnmode"
 
+# function key behavior change like window
+echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+sudo update-initramfs -u -k all
+
 emacs README.md
 
 
